7e418057594cc3da2f5f46e9d5baca3e0076e620
unknown <yusha814@gmail.com>
2023-09-01 12:03:59 +0600

    one time dummy data added

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import {createPost} from "./controllers/posts.js";
 import {verifyToken} from "./middleware/auth.js";
 import User from "./models/User.js"
 import Post from "./models/Post.js"
+import {users, posts} from "./data/index.js"
 
 
 
@@ -63,5 +64,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(()=>{
     app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`));
+
+    //ADD DUMMY DATA ONE TIME
+    //User.insertMany(users);
+    //Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
 
