e96bd967af634f71271e9ff67f2b3b1b98d745f0
unknown <yusha814@gmail.com>
2023-09-05 12:54:27 +0600

    advertise page and friends list added

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -30,7 +32,13 @@ const HomePage = () => {
           <PostsWidget userId={_id} />
         </Box>
         {/* FRIENDS LIST WILL ONLY SHOW IN DESKTOP */}
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id}/>
+          </Box>
+        )}
       </Box>
     </Box>
   );
---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,36 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advertise"
+        src="http://localhost:3001/assets/adv.png"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Mr Fictionist</Typography>
+        <Typography color={medium}>@mrfictionistyt</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        An ordinary man who loves gaming and anime.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+export default AdvertWidget;
